3381761e96616cb85c3af1c2a4b84440f190f297
Unknown <davidsourod@gmail.com>
2017-10-12 17:39:12 -0300

    Basic Menu

---
+++ Assets/Scripts/PlayButtonScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PlayButtonScript : MonoBehaviour {
+	private Button b;
+
+	// Use this for initialization
+	void Start () {
+		b = GetComponent<Button>();
+		b.onClick.AddListener (() =>ChangeScene());
+	}
+
+	void ChangeScene(){
+		SceneManager.LoadScene ("GameTest", LoadSceneMode.Single);
+	}
+}
