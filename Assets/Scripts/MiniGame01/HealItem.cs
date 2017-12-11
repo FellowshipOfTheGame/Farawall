91bf185124fca121d89f493c14dc3197ba1a739f
Unknown <giulianolourencon@usp.br>
2017-12-11 19:06:59 -0200

    Minigame01 update3

---
+++ Assets/Scripts/MiniGame01/HealItem.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealItem : MonoBehaviour {
+
+	public int hp;
+
+	void OnTriggerEnter2D(Collider2D col){
+		if (col.gameObject.tag == "Player") {
+			col.GetComponent<Neith> ().Heal (hp);
+			Destroy (gameObject);
+		}
+	}
+
+}
