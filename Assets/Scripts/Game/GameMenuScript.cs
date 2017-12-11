6f31b040d3b1023dc0aba57395c457d2807e6322
Unknown <davidsourod@gmail.com>
2017-12-11 19:15:09 -0200

    Map and level1 Scene

--- Assets/Scripts/Game/GameMenuScript.cs
+++ Assets/Scripts/Game/GameMenuScript.cs
@@ -5,19 +5,28 @@ using UnityEngine.UI;
 
 public class GameMenuScript : MonoBehaviour {
     public GameObject menu;
-    private bool active;
+    public GameObject map;
+    private bool menuActive;
+    private bool mapActive;
 
 	// Use this for initialization
 	void Start () {
-        active = false;
-        menu.SetActive(active);
+        menuActive = false;
+        mapActive = false;
+        menu.SetActive(menuActive);
+        map.SetActive(mapActive);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKeyDown(KeyCode.M)){
-            active = !active;
-            menu.SetActive(active);
+        if (Input.GetKeyDown(KeyCode.Escape) && !mapActive)
+        {
+            menuActive = !menuActive;
+            menu.SetActive(menuActive);
+        }else if (Input.GetKeyDown(KeyCode.M) && !menuActive)
+        {
+            mapActive = !mapActive;
+            map.SetActive(mapActive);
         }
 	}
 }
