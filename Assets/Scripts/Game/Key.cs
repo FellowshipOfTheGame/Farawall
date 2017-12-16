e1c4785aecade4e27b28aaa5e05d29b398c69f25
Unknown <davidsourod@gmail.com>
2017-12-16 20:49:02 -0200

    Missing file

---
+++ Assets/Scripts/Game/Key.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : Interactable {
+	public Door door;
+
+	public override void Interact (){
+		if(nearPlayer) door.Unlock ();
+	}
+
+	public override void Close(){
+		Destroy (this.gameObject);
+	}
+
+	void OnTriggerEnter(Collider other){
+		if(other.tag == "Player")
+		nearPlayer = true;
+	}
+
+	void OnTriggerExit(Collider other){
+		if(other.tag == "Player")
+		nearPlayer = false;
+	}
+}
---
+++ Assets/Scripts/Game/Trap.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap : MonoBehaviour {
+	public Transform startPosition;
+	public Transform endPosition;
+	public Transform spears;
+	public float speed;
+	private bool active = false;
+	private bool deactivate = false;
+	private float startTime;
+	private float distance;
+	private Vector3 start;
+	private Vector3 end;
+
+	void Start () {
+		spears.position = startPosition.position;
+		distance = Vector3.Distance (startPosition.position, endPosition.position);
+	}
+
+	void Update () {
+		if (active) {
+			float coveredDistance = (Time.time - startTime) * speed;
+			float currentPosition = coveredDistance / distance;
+			spears.position = Vector3.Lerp (start, end, currentPosition);
+			if (spears.position == end) {
+				if (deactivate && end == startPosition.position) {
+					deactivate = false;
+					active = false;
+				}
+				Vector3 aux;
+				aux = end;
+				end = start;
+				start = aux;
+				startTime = Time.time;
+			}
+		}
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.tag == "Player") {
+			start = startPosition.position;
+			end = endPosition.position;
+			active = true;
+		}
+	}
+
+	void OnTriggerExit(Collider other){
+		if (other.tag == "Player") {
+			deactivate = true;
+		}
+	}
+}
