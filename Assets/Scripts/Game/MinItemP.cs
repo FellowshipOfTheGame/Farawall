57302e6066b35ba3701aa3df8797cbbb7d46d017
Unknown <will.leidan@gmail.com>
2018-05-13 15:04:45 -0300

    Add Stuff

--- Assets/Scripts/Game/MinItemP.cs
+++ Assets/Scripts/Game/MinItemP.cs
@@ -1,16 +1,24 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class MinItemP : MonoBehaviour {
+public class MinItemP : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    GameObject message;
+
+    // Use this for initialization
+    void Start () {
+        message = this.transform.GetChild(0).gameObject;
+        message.SetActive(false);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData) {
+        message.SetActive(true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData) {
+        message.SetActive(false);
+    }
 }
